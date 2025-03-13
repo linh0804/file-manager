82031764798203cfd6b6791ee91c0fb4440896d8
ngatngay <ngatngay@ngatngay.net>
2025-03-13 10:42:14 +0700

    update

--- db/index.php
+++ db/index.php
@@ -2,24 +2,24 @@
 
 function adminer_object()
 {
-    require_once "./plugins/plugin.php";
+    include_once "./plugins/plugin.php";
 
     foreach (glob("plugins/*.php") as $filename) {
-        require_once "./$filename";
+        include_once "./$filename";
     }
 
     if (file_exists("config.php")) {
-        require_once "config.php";
+        include_once "config.php";
     }
 
     $plugins = [
         new AdminerDatabaseHide(["mysql", "sys", "information_schema", "performance_schema"]),
         new AdminerSimpleMenu(),
-        new AdminerCollations(["utf8mb4_general_ci",  "utf8mb4_vietnamese_ci"]),
+        new AdminerCollations(["ascii_general_ci", "utf8mb4_general_ci",  "utf8mb4_vietnamese_ci"]),
         new AdminerTheme('default-green'),
     ];
 
     return new AdminerPlugin($plugins);
 }
 
-require_once './adminer.php';
+require './adminer.php';

7f53220a9333fcbab3da674e5e68dbfb21ee759c
ngatngay <ngatngay@ngatngay.net>
2025-04-18 08:33:09 +0700

    php 8.2.0

--- db/adminer-plugins.php
+++ db/adminer-plugins.php
@@ -3,5 +3,5 @@
 return [
     new AdminerDatabaseHide(["mysql", "sys", "information_schema", "performance_schema"]),
     //new AdminerSimpleMenu(),
-    new AdminerCollations(["utf8mb4_general_ci",  "utf8mb4_vietnamese_ci"]),
+    new AdminerCollations(["utf8mb4_general_ci",  "utf8mb4_vietnamese_ci", "utf8mb4_uca1400_ai_ci"]),
 ];
--- db/adminer-plugins/AdminerCollations.php
+++ db/adminer-plugins/AdminerCollations.php
@@ -16,7 +16,7 @@ class AdminerCollations
     /**
      * @param array $characterSets Array of allowed character sets.
      */
-    public function __construct(array $characterSets = ["utf8mb4_general_ci", "ascii_general_ci"])
+    public function __construct(array $characterSets = ["utf8mb4_general_ci"])
     {
         $this->characterSets = $characterSets;
     }
@@ -38,58 +38,27 @@ class AdminerCollations
 
                 const characterSets = [
                     <?php
-                        echo "'(" . Adminer\lang('collation') . ")'";
-
+                        $data = [];
                         foreach ($this->characterSets as $characterSet) {
-                            echo ", '" . $characterSet . "'";
+                            $data[] = "'" . $characterSet . "'";
                         }
+                        echo implode(',', $data);
                     ?>
                 ];
 
-                document.addEventListener("DOMContentLoaded", init, false);
-
-                function init() {
-                    var selects = document.querySelectorAll("select[name='Collation'], select[name*='collation']");
-
-                    for (var i = 0; i < selects.length; i++) {
-                        replaceOptions(selects[i]);
-                    }
-                }
-
-                function replaceOptions(select) {
-                    var selectedSet = getSelectedSet(select);
-                    var html = '';
-                    var hasSelected = false;
+                document.addEventListener("DOMContentLoaded", function () {
+                    var selects = document.querySelector("datalist[id='collations']");
+                    var html = "";
 
                     for (var i = 0; i < characterSets.length; i++) {
-                        if (characterSets[i] === selectedSet) {
-                            hasSelected = true;
-                            html += '<option selected="selected">' + characterSets[i] + '</option>';
-                        } else {
-                            html += '<option>' + characterSets[i] + '</option>';
-                        }
+                        html += '<option>' + characterSets[i];
                     }
 
-                    if (!hasSelected && selectedSet !== "") {
-                        html += '<option selected="selected">' + selectedSet + '</option>';
+                    if (selects) {
+                        selects.innerHTML = html;
                     }
-
-                    select.innerHTML = html;
-                }
-
-                function getSelectedSet(select) {
-                    var options = select.getElementsByTagName("option");
-
-                    for (var i = 0; i < options.length; i++) {
-                        if (options[i].selected) {
-                            return options[i].innerHTML.trim();
-                        }
-                    }
-
-                    return "";
-                }
+                }, false);
             })(document);
-
         </script>
 
         <?php

ae0c6e9525c5721e2738fed8601f1af0bca29d58
ngatngay <25657732+pmtpro@users.noreply.github.com>
2026-06-26 21:01:22 +0700

    update

--- db/adminneo-config.php
+++ db/adminneo-config.php
@@ -3,9 +3,11 @@
 return [
     'hiddenDatabases' => '__system',
     'visibleCollations' => [
-        "utf8mb4_general_ci",
-        "utf8mb4_vietnamese_ci",
-        "utf8mb4_uca1400_ai_ci"
+        'utf8mb4_general_ci',
+        'utf8mb4_vietnamese_ci',
+        'utf8mb4_uca1400_ai_ci'
     ],
-    "defaultPasswordHash" => "",
+    'defaultServer' => '127.0.0.1',
+    'defaultPasswordHash' => '',
+    'sqlAutocompletion' => false,
 ];

7b20933bee69d18bbcde77e3cccc11ae9bd8f49f
ngatngay <25657732+pmtpro@users.noreply.github.com>
2026-07-02 10:31:44 +0700

    update

--- logout.php
+++ logout.php
@@ -1,9 +1,13 @@
 <?php
 
 define('ACCESS', true);
-
 require __DIR__ . '/_init.php';
 
-setcookie(APP_NAME . '_auth', '', 0);
+setcookie(
+    APP_NAME . '_auth',
+    'logout',
+    time() - 3600,
+    '/'
+);
 
 redirect(action_link('index'));

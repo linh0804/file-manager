c32c4de1f5d11081be09d186abef12af032708cb
me <me@mail.com>
2026-08-06 15:16:22 +0700

    update

--- multi_zip.php
+++ multi_zip.php
@@ -12,9 +12,11 @@ function multi_zip($dir, $entrys, $file, $isDelete = false)
     }
 
     $zip = new Zip();
+
     if ($zip->open($file, ZipArchive::CREATE) !== true) {
         return false;
     }
+
     foreach ($entrys as $entry) {
         $path = "$dir/$entry";
         $zip->add($path, $dir);
@@ -27,6 +29,7 @@ function multi_zip($dir, $entrys, $file, $isDelete = false)
             }
         }
     }
+
     $zip->close();
 
     if ($isDelete) {
@@ -36,10 +39,57 @@ function multi_zip($dir, $entrys, $file, $isDelete = false)
     return true;
 }
 
-$name = !empty($_GET['name']) ? $_GET['name'] : null;
+///
 
-$site_title = 'Nén zip';
+$name = $_GET['name'] ?? '';
 $curr_path = process_directory($curr_path);
+$site_title = 'Nén zip';
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['name']) || empty($_POST['path_new'])) {
+        response_api([
+            'msg' => 'Chưa nhập đầy đủ thông tin'
+        ]);
+    } elseif (
+        isset($_POST['is_delete'])
+        && process_directory($_POST['path_new']) == $curr_path . '/' . ($name ?? '')
+    ) {
+        response_api([
+            'msg' => 'Nếu chọn xóa thư mục bạn không thể lưu tập tin nén ở đó'
+        ]);
+    } elseif (file_name_valid($_POST['name'])) {
+        response_api([
+            'msg' => 'Tên tập tin zip không hợp lệ'
+        ]);
+    } elseif (file_exists(process_directory($_POST['path_new'] . '/' . $_POST['name']))) {
+        response_api([
+            'msg' => 'Tập tin đã tồn tại, vui lòng đổi tên!'
+        ]);
+    } elseif (
+        !multi_zip(
+            $curr_path,
+            $entries,
+            process_directory($_POST['path_new'] . '/' . $_POST['name']),
+            isset($_POST['is_delete'])
+        )
+    ) {
+        response_api([
+            'msg' => 'Nén zip thất bại'
+        ]);
+    } else {
+        response_api([
+            'status' => true,
+            'msg' => 'Thành công',
+            'reload' => isset($_POST['is_delete']),
+            'reload_after_close' => true
+        ]);
+    }
+
+    exit;
+}
+
+///
+
 $entry_checkbox = '';
 $entry_html_list = '<ul class="list">';
 
@@ -52,50 +102,24 @@ foreach ($entries as $e) {
 
 $entry_html_list .= '</ul>';
 
-require SITE_HEADER;
-
 echo '<div class="title">' . $site_title . '</div>';
 
-if (isset($_POST['submit']) && isset($_POST['is_action'])) {
-    echo '<div class="notice_failure">';
-
-    if (empty($_POST['name']) || empty($_POST['path_new'])) {
-        echo 'Chưa nhập đầy đủ thông tin';
-    } elseif (isset($_POST['is_delete']) && process_directory($_POST['path_new']) == $curr_path . '/' . ($name ?? '')) {
-        echo 'Nếu chọn xóa thư mục bạn không thể lưu tập tin nén ở đó';
-    } elseif (file_name_valid($_POST['name'])) {
-        echo 'Tên tập tin zip không hợp lệ';
-    } elseif (file_exists(process_directory($_POST['path_new'] . '/' . $_POST['name']))) {
-        echo 'Tập tin đã tồn tại, vui lòng đổi tên!';
-    } elseif (!multi_zip($curr_path, $entries, process_directory($_POST['path_new'] . '/' . $_POST['name']), isset($_POST['is_delete']))) {
-        echo 'Nén zip thất bại';
-    } else {
-        redirect(act_link('index', ['path' => $curr_path]));
-    }
-
-    echo '</div>';
-}
-
 echo $entry_html_list;
+
 echo '<div class="list">
-        <span>' . file_print_path($curr_path, true) . '</span><hr/>
-        <form action="' . get_curr_url_esc() . '" method="post">
-            <span class="bull">&bull; </span>Tên tập tin nén:<br/>
-            <input type="text" name="name" value="' . ($_POST['name'] ?? 'archive-' . date('Ymd-His') . '.zip') . '" size="18"/><br/>
-            <span class="bull">&bull; </span>Đường dẫn lưu:<br/>
-            <input type="text" name="path_new" value="' . ($_POST['path_new'] ?? $curr_path) . '"/><br/>
-            <input type="checkbox" name="is_delete" value="1"' . (isset($_POST['is_delete']) ? ' checked="checked"' : null) . '/> Xóa nguồn<br/>
-            <input type="hidden" name="is_action" value="1"/>';
+    <span>' . file_print_path($curr_path, true) . '</span><hr/>
+
+    <form data-ajax action="' . get_curr_url_esc() . '" method="post">
+        <span class="bull">&bull; </span>Tên tập tin nén:<br/>
+        <input type="text" name="name" value="' . ($_POST['name'] ?? 'archive-' . date('Ymd-His') . '.zip') . '" size="18"/><br/>
+
+        <span class="bull">&bull; </span>Đường dẫn lưu:<br/>
+        <input type="text" name="path_new" value="' . ($_POST['path_new'] ?? $curr_path) . '"/><br/>
+
+        <input type="checkbox" name="is_delete" value="1"' . (isset($_POST['is_delete']) ? ' checked="checked"' : null) . '/> Xóa nguồn<br/>';
 
 echo $entry_checkbox;
 
 echo '<input type="submit" name="submit" value="Nén"/>
-        </form>
-    </div>';
-
-echo '<div class="title">Chức năng</div>
-    <ul class="list">
-        <li><img src="icon/list.png" alt=""/> <a href="' . act_link('index', ['path' => $curr_path]) . '">Danh sách</a></li>
-    </ul>';
-
-require SITE_FOOTER;
+    </form>
+</div>';

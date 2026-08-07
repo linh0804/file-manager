0a21153e093d9fa65f5c415adf57145c82e1a6aa
me <me@mail.com>
2026-08-07 15:17:22 +0700

    update

--- js/app.js
+++ js/app.js
@@ -144,3 +144,159 @@ $(".copy-button").click(function (e) {
             $.notify("Lỗi: " + err);
         });
 });
+
+// FILE UPLOAD
+
+$(function () {
+    const files = [];
+    let uploading = false;
+
+    $(document).on("click", "#file-upload #button-choose", function (e) {
+        e.preventDefault();
+        const $fileUpload = $(this).closest("#file-upload");
+        $fileUpload.find("#files").val("");
+        $fileUpload.find("#files").click();
+    });
+
+    $(document).on("click", "#file-upload #button-reset", function (e) {
+        e.preventDefault();
+        const $fileUpload = $(this).closest("#file-upload");
+
+        if (uploading) {
+            alert("Đang upload!");
+            return;
+        }
+
+        files.length = 0;
+        $fileUpload.find("#file-list").empty();
+    });
+
+    $(document).on("change", "#file-upload #files", function () {
+        const $fileUpload = $(this).closest("#file-upload");
+        const $fileList = $fileUpload.find("#file-list");
+
+        $fileList.empty();
+
+        files.push(...Array.from(this.files));
+        for (let i = 0; i < files.length; i++) {
+            $fileList.append(`
+                <div class="file-upload list" data-id="${i}" style="font-size: small">
+                    <span class="bull"></span> ${files[i].name}<br>
+                    <span style="color: #FF00FF">(${(files[i].size / (1024 * 1024)).toFixed(2)} MB)</span>
+                    <span class="result"></span>
+                </div>
+            `);
+        }
+    });
+
+    $(document).on("click", "#file-upload #button-upload", async function (e) {
+        e.preventDefault();
+        const $fileUpload = $(this).closest("#file-upload");
+
+        if (!files.length) {
+            alert("Chưa chọn file!");
+            return;
+        }
+
+        if (uploading) {
+            alert("Đang upload!");
+            return;
+        }
+
+        const uploadItems = [];
+
+        $fileUpload.find(".file-upload").each(function () {
+            const $item = $(this);
+            const id = $item.data("id");
+
+            if (files[id]) {
+                uploadItems.push({
+                    file: files[id],
+                    result: $item.find(".result")
+                });
+            }
+        });
+
+        uploading = true;
+        NProgress.start();
+
+        try {
+            const action = $fileUpload.attr("action");
+            for (const item of uploadItems) {
+                await upload(item.file, item.result, action);
+            }
+        } finally {
+            uploading = false;
+            NProgress.done();
+        }
+    });
+
+    function upload(file, result, action) {
+        return new Promise(function (resolve) {
+            console.log(file.name);
+
+            const formData = new FormData();
+            formData.append("file", file);
+
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", action);
+
+            let lastLoaded = 0;
+            let lastTime = performance.now();
+
+            xhr.upload.onprogress = function (e) {
+                if (e.lengthComputable) {
+                    const now = performance.now();
+                    const elapsed = (now - lastTime) / 1000;
+                    const uploaded = e.loaded - lastLoaded;
+                    const speed = elapsed > 0 ? uploaded / elapsed : 0;
+                    const percent = Math.min(100, Math.round((e.loaded / e.total) * 100));
+                    const speedKB = (speed / 1024).toFixed(0);
+
+                    result.html("<span style=\"color: orange\">[" + percent + "%] (" + speedKB + " kb/s)</span>");
+
+                    lastLoaded = e.loaded;
+                    lastTime = now;
+                }
+            };
+
+            xhr.onload = function () {
+                try {
+                    const res = JSON.parse(xhr.responseText);
+
+                    if (res.error) {
+                        result.html("<span style=\"color:red\">" + res.error + "</span>");
+                        alert("Tải lên thất bại: " + file.name);
+                    } else if (xhr.status < 200 || xhr.status >= 300) {
+                        result.html("<span style=\"color:red\">Thất bại!</span>");
+                        alert("Tải lên thất bại: " + file.name);
+                    } else {
+                        result.html("<span style=\"color:green\">OK!</span>");
+                    }
+                } catch (e) {
+                    result.html("<span style=\"color:red\">Thất bại!</span>");
+                    alert("Tải lên thất bại: " + file.name);
+                    console.log(e);
+                }
+            };
+
+            xhr.onerror = function () {
+                result.html("<span style=\"color:red\">Lỗi kết nối!</span>");
+                alert("Tải lên thất bại: " + file.name);
+            };
+
+            xhr.onloadend = function () {
+                resolve();
+            };
+
+            try {
+                xhr.send(formData);
+            } catch (e) {
+                result.html("<span style=\"color:red\">Thất bại!</span>");
+                alert("Tải lên thất bại: " + file.name);
+                console.log(e);
+                resolve();
+            }
+        });
+    }
+});
